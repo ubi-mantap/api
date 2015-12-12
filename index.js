510469cdcd8c5e0aa9ccff11d51ae503e0579dc1
Bobby Priambodo <bobby.priambodo@gmail.com>
2015-12-12 12:48:25 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.PORT || 5000);
+
+app.get('/', (req, res) => {
+  let a = 10;
+  res.send(`hello! a is ${a}`);
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Node app is running on port ${app.get('port')}`);
+});
