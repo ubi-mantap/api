81ae6435ec202b4c7cd3e16f2e132661dbabe1dd
Bobby Priambodo <bobby.priambodo@gmail.com>
2015-12-15 08:58:12 +0700

    Register done

--- handlers/register.js
+++ handlers/register.js
@@ -1,5 +1,20 @@
+'use strict';
+
+const logger = require('../libs/logger');
+
 module.exports = function factory(models) {
   return function register(req, res) {
-    res.json(models);
+    logger.debug('[Register Handler] Got request.', req.body);
+
+    const username = req.body.username;
+
+    models.User
+      .new(username)
+      .then(() => {
+        res.json({ ok: true });
+      })
+      .catch(() => {
+        res.json({ ok: false, message: 'Register failed. Try again.' });
+      });
   };
 };
