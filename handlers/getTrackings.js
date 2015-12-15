0953d5194ae4745c4d01702b368ea52ba6ea53e1
Bobby Priambodo <bobby.priambodo@gmail.com>
2015-12-15 11:20:52 +0700

    Handle getTrackings done

--- handlers/getTrackings.js
+++ handlers/getTrackings.js
@@ -1,5 +1,21 @@
+'use strict';
+
+const logger = require('../libs/logger');
+
 module.exports = function factory(models) {
   return function getTrackings(req, res) {
-    res.json(models);
+    logger.debug('[GetTrackings Handler] Got request.', req.query);
+
+    const username = req.query.username;
+
+    models.Tracking.findTrackings(username)
+      .then(result => {
+        logger.debug('[GetTrackings Handler] Get trackings success.');
+        res.json({ ok: true, trackings: result });
+      })
+      .catch(err => {
+        logger.error('[GetTrackings Handler] Error getting trackings.', err);
+        res.json({ ok: false, trackings: [] });
+      });
   };
 };
