e6fa7a7bc28de61217bb1a05d31e2c9f8ae57b8c
Bobby Priambodo <bobby.priambodo@gmail.com>
2015-12-26 20:49:20 +0700

    Remove weather-test route

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const request = require('request');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -25,21 +24,6 @@ app.post('/register', handlers.register);
 app.get('/trackings', handlers.getTrackings);
 app.get('/trackers', handlers.getTrackers);
 app.get('/notifications', handlers.getNotifications);
-
-// TODO: move this to handlers.log
-app.get('/weather-test', (req, res) => {
-  const WEATHER_API_KEY = '79a4f0e5d011f84644fee1cecc5e03ae';
-  const lat = '-6.328535';
-  const long = '106.7387273';
-  const weatherApiUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${WEATHER_API_KEY}`;
-  request(weatherApiUrl, (err, response, body) => {
-    let weatherData = JSON.parse(body);
-    res.json({
-      weather: weatherData.weather[0].main,
-      city: weatherData.name
-    });
-  });
-});
 
 app.listen(app.get('port'), () => {
   logger.info(`Node app is running on port ${app.get('port')}`);
